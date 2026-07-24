d3a3a5c29fa5163ca10bf83f0bab4d6892efad4e
Olena Mustafina <toysamiy.mail@gmail.com>
2026-07-24 15:22:12 +0300

    feat: Install focus-trap integrations and complete mobile menu loop

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue";
+import { ref, nextTick, watch } from "vue";
+import { useFocusTrap } from "@vueuse/integrations/useFocusTrap";
 import logo from "@/assets/icons/logo.svg";
 import cartIcon from "@/assets/icons/icon-cart.svg";
 import menuIcon from "@/assets/icons/icon-menu.svg";
@@ -9,9 +10,22 @@ import avatar from "@/assets/images/image-avatar.png";
 const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
 const isMenuOpen = ref(false);
 
+const mobileMenuRef = ref(null);
+
+const { activate, deactivate } = useFocusTrap(mobileMenuRef);
+
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
 };
+
+watch(isMenuOpen, async (isOpen) => {
+  if (isOpen) {
+    await nextTick();
+    activate();
+  } else {
+    deactivate();
+  }
+});
 </script>
 
 <template>
@@ -64,7 +78,10 @@ const toggleMenu = () => {
         aria-hidden="true"
       ></div>
 
-      <nav class="absolute top-0 left-0 h-full w-2/3 bg-white p-6 shadow-2xl">
+      <nav
+        ref="mobileMenuRef"
+        class="absolute top-0 left-0 h-full w-2/3 bg-white p-6 shadow-2xl"
+      >
         <button
           @click="toggleMenu"
           class="block focus-visible:ring-2 focus-visible:ring-orange-primary rounded-sm p-1 focus:outline-none mb-12"
